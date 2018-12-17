bf099071cff0eea3c6c75d94c27e4dddd8e228f5
Phatjira Suknaboon <42989727+Phatjira@users.noreply.github.com>
2018-12-17 22:06:01 +0700

    Update year.py

--- calculate/year.py
+++ calculate/year.py
@@ -3,10 +3,9 @@ import pygal
 from pygal.style import RedBlueStyle
 def chart():
     """ years """
-    data = pd.read_csv("2504_2533n.csv")
     line_chart = pygal.Bar()
     line_chart.title = 'Average weather in a year 2504-2533'
-    line_chart.x_labels = map('Month', [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec])
+    line_chart.x_labels = map(str, range(2002, 2013))
     line_chart.add('Maxtemp', [32, 32.7, 33.7, 34.9, 34, 33.1, 32.7, 32.5, 32.3, 32, 31.6, 31.3])
     line_chart.add('Mintemp', [21, 23.3, 24.9, 26.1, 25.6, 25.4, 25, 24.9, 24.6, 24.3, 23.1, 20.8])
     line_chart.add('Water', [9.1, 29.9, 28.6, 64.7, 220.4, 149.3, 154.5, 196.7, 344.2, 241.6, 48.1, 9.7])
